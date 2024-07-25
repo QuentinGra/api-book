bb38823fea0d5c4c58758c3b1bdb15b63630256d
QuentinGra <pilou4260042@gmail.com>
2024-07-25 11:57:08 +0200

    feat(edition): add findAllWithPagination

--- src/Repository/EditionRepository.php
+++ src/Repository/EditionRepository.php
@@ -5,15 +5,31 @@ namespace App\Repository;
 use App\Entity\Edition;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @extends ServiceEntityRepository<Edition>
  */
 class EditionRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
+    public function __construct(
+        ManagerRegistry $registry,
+        private readonly PaginatorInterface $pagination,
+    ) {
         parent::__construct($registry, Edition::class);
+    }
+
+    public function findAllWithPagination(int $page, ?int $limit): PaginationInterface
+    {
+        $query = $this->createQueryBuilder('u')
+            ->orderBy('u.createdAt', 'ASC');
+
+        return $this->pagination->paginate(
+            $query->getQuery(),
+            $page,
+            $limit
+        );
     }
 
     //    /**
